39afbbfe22eafef7ec98d0d9618a6df5733c836c
Irina <ireana.melnikova@gmail.com>
2022-08-06 14:47:56 +0300

    TextChoices for ticket states implemented.

--- tickets/models.py
+++ tickets/models.py
@@ -6,11 +6,21 @@ def upload_to(instance, filename):
     return f'images/{filename}'
 
 
+class TicketState(models.TextChoices):
+    UNRESOLVED = 'unresolved'
+    RESOLVED = 'resolved'
+    FROZEN = 'frozen'
+
+
 class Ticket(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    state = models.CharField(max_length=10, default='unresolved')
+    state = models.CharField(
+        max_length=10,
+        choices=TicketState.choices,
+        default=TicketState.UNRESOLVED,
+    )
     ticket_photo = models.ImageField(upload_to=upload_to, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
